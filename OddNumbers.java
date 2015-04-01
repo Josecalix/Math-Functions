9db1aa2575f503c3c09e3eebba73776b06e91e5e
Jose Calix <jbcalix@hotmail.com>
2015-03-31 21:12:23 -0400

    initial commit

---
+++ OddNumbers.java
@@ -0,0 +1,14 @@
+ 
+ public class OddNumbers {
+
+	public static void main(String[] args) {
+		int oddNumber;
+		
+		for (int start = 0; start <100; start ++){
+			oddNumber = 1 + 2*start;
+			System.out.println(oddNumber);
+		}		
+
+	}
+
+}
